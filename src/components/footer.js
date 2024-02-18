a22dc6d993ca11567e24b46d9f70de682bd0bec9
Kriti <kritithawaria@gmail.com>
2024-02-18 16:46:35 +0530

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+// import React from "react";
+// import "./Footer.css"; // Import the CSS file for footer styling
+
+function Footer() {
+  return (
+    <footer className="footer-container">
+      <p className="footer-text">© 2024 Your Chatbot Company</p>
+    </footer>
+  );
+}
+
---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+// const Header = () => {
+//   return <h1>Diva Guard</h1>;
+// };
+
+// export default Header;
+
+// Header.js
+
+// import React from "react";
+// import "App.css"; // Import the CSS file
+
+function Header() {
+  return (
+    <header className="header-container">
+      <h1 className="title">DivaBot</h1>
+    </header>
+  );
+}
+
+export default Header;
